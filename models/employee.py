b29d15a8bcceb6edab6785a84855eadc4ed8230d
solomonferede1 <solomonf1227@gmail.com>
2024-08-27 21:09:11 +0300

    Employe databse is created using SQLAlchemy

--- models/employee.py
+++ models/employee.py
@@ -3,7 +3,7 @@
 
 
 from base_model import Base
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, Numeric, DateTime
 
 
 class Employee(Base):
@@ -12,3 +12,10 @@ class Employee(Base):
     __tablename__ = 'employee'
 
     id = Column(Integer, nullable=False, primary_key=True)
+    first_name = Column(String(60))
+    last_name = Column(String(60))
+    role = Column(String(60))
+    email = Column(String(60), unique=True, nullable=True)
+    phone = Column(String(60), unique=True, nullable=False)
+    salary = Column(Numeric(10, 2))
+    date_hired = Column(DateTime)
