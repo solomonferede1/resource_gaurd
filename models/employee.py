fe7fdabea8652e860ef4c0a018c63b09818242ac
Solomon <solomonf1227@gmail.com>
2024-08-28 15:15:58 +0300

    Employee model creation and storage

--- models/employee.py
+++ models/employee.py
@@ -2,20 +2,25 @@
 """Employee model - create and manage the organization employee date """
 
 
-from base_model import Base
-from sqlalchemy import Column, String, Integer, Numeric, DateTime
+from datetime import datetime
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Numeric, DateTime
 
 
-class Employee(Base):
+class Employee(BaseModel, Base):
     '''Employee class - mapped with employee table'''
 
     __tablename__ = 'employees'
 
-    id = Column(Integer, nullable=False, primary_key=True)
     first_name = Column(String(60))
     last_name = Column(String(60))
     role = Column(String(60))
     email = Column(String(60), unique=True, nullable=True)
     phone = Column(String(60), unique=True, nullable=False)
     salary = Column(Numeric(10, 2))
-    date_hired = Column(DateTime)
+    date_hired = Column(DateTime, default=datetime.utcnow())
+
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
