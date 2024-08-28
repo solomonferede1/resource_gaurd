fc9ae2ccb7cf05b3294db2499c917f70c0aa4161
solomonferede1 <solomonf1227@gmail.com>
2024-08-28 23:12:17 +0300

    Setup the database and user

--- setup_mysql_rs.sql
+++ setup_mysql_rs.sql
@@ -5,3 +5,8 @@ CREATE USER IF NOT EXISTS 'RG_user'@'localhost' IDENTIFIED BY 'RG_password';
 GRANT ALL PRIVILEGES ON `RG_db`.* TO 'RG_user'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'RG_user'@'localhost';
 FLUSH PRIVILEGES;
+
+
+-- Environment variables
+
+-- MYSQL_USER='RG_user' MYSQL_PWD='RG_password' MYSQL_HOST='localhost' MYSQL_DB='RG_db'
