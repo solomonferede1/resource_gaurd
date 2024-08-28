6826458f0c15682108fdbcf58a4f0d890110a1e3
solomonferede1 <solomonf1227@gmail.com>
2024-08-28 23:14:12 +0300

    Products model/table for the project

---
+++ models/product.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''Products table/model'''
+
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String, Numeric, DateTime, Integer, ForeignKey
+from datetime import datetime
+
+
+class Product(BaseModel, Base):
+    '''Product model class representing products in the inventory.'''
+
+    __tablename__ = 'products'
+
+    product_name = Column(String(60), nullable=False)
+    product_type = Column(String(60), nullable=False)
+    quantity = Column(Integer, nullable=False)
+    price = Column(Numeric(10, 2), nullable=False)
+    production_date = Column(DateTime, nullable=False, default=datetime.utcnow)
+    deleted_at = Column(DateTime, nullable=True)  # Soft delete column
+
+    category_id = Column(Integer, ForeignKey('catagories.id'), nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        '''Call super basemodel class to initialize common att'''
+        super().__init__(*args, **kwargs)
