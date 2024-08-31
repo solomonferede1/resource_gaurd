fcd5e1000ed99c748953c9783d6af6bcf7507239
solomonferede1 <solomonf1227@gmail.com>
2024-08-31 08:26:46 +0300

    attendance api

--- models/attendance.py
+++ models/attendance.py
@@ -6,7 +6,7 @@ from models.base_model import Base, BaseModel
 from sqlalchemy import Column, Integer, Numeric,\
     Boolean, ForeignKey, DateTime, Date, Computed
 from sqlalchemy.orm import relationship
-from datetime import date
+from datetime import date, datetime
 
 class Attendance(BaseModel, Base):
     '''Attendance'''
@@ -14,8 +14,8 @@ class Attendance(BaseModel, Base):
     __tablename__ = 'attendances'
 
     attendance_date = Column(Date, default=date.today)
-    check_in_time = Column(DateTime, nullable=True)
-    check_out_time = Column(DateTime, nullable=True)
+    check_in_time = Column(DateTime, default=datetime.now())
+    check_out_time = Column(DateTime, default=datetime.now())
     total_work_hours = Column(Numeric(4, 2),
                               Computed("TIMESTAMPDIFF(Hour,\
                                        check_in_time, check_out_time)",
